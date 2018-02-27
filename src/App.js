1bb523026f33c004d47d90e589f288d4000e30ab
Caroline Nguyen <caroline.nguyen@outlook.com>
2018-02-26 23:34:32 -0600

    m02c08 - completed

--- src/App.js
+++ src/App.js
@@ -7,21 +7,52 @@ class App extends Component {
     super(props);
     this.state ={
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
-      ]
+        { description: 'Walk the cat', isCompleted: true, id: 0 },
+        { description: 'Throw the dishes away', isCompleted: false, id: 1 },
+        { description: 'Buy new dishes', isCompleted: false, id: 2 }
+      ],
+      id: 3,
+      isCompleted: false,
+      newTodoDescription: ''
     };
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { id: this.state.id, description: this.state.newTodoDescription, isCompleted: this.state.isCompleted };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '', id: this.state.id + 1 });
+  }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
+  deleteTodo(id) {
+    const remainingToDos = this.state.todos.filter(todo => { return todo.id !== id; });
+    this.setState({ todos: remainingToDos });
   }
 
   render() {
     return (
       <div className="App">
         <ul>
-          { this.state.todos.map( (todo, index) =>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+        { this.state.todos.map( (todo, id) =>
+          <ToDo key={ id } id={ todo.id } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(id) } onDelete={ this.deleteTodo } />
         )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" value="Add Todo" />
+        </form>
       </div>
     );
   }
